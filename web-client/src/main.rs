6de0b37e3ac0935ae7ce5cdc16e3f492de1d7a77
John Shaughnessy <john@johnshaughnessy.com>
2023-11-06 23:00:11 -0500

    Add themes to web-client

--- web-client/src/main.rs
+++ web-client/src/main.rs
@@ -1,5 +1,6 @@
 mod models;
 
+use chrono::TimeZone;
 use gloo_net::http::Request;
 use models::CreateWeightPayload;
 use models::Weight;
@@ -100,61 +101,71 @@ fn App() -> Html {
         }
     };
 
-    // html! {
-    //     <div>
-    //         <button onclick={Callback::from(move |_| fetch_weights.emit(()))}>{ "Fetch Weights" }</button>
-    //         <button onclick={create_weight}>{ "Create Weight" }</button>
-    //         <div>
-    //         {
-    //             for weights.iter().map(|weight| {
-    //                 let delete_callback = make_delete_callback(weight.weight_id);
-    //                 let edit_callback = make_edit_callback(weight.weight_id);
+    let themes = vec!["gruvbox", "light-mode", "dark-mode", "warm", "cool"];
+    let theme_index = use_state(|| 0);
 
-    //                 html! {
-    //                     <div>
-    //                         <p>{ format!("weight_id: {}", weight.weight_id) }</p>
-    //                         <p>{ format!("measured_at: {}", weight.measured_at) }</p>
-    //                         <p>{ format!("weight_kg: {}", weight.weight_kg) }</p>
-    //                         <button onclick={delete_callback}>{ "Delete" }</button>
-    //                         <button onclick={edit_callback}>{ "Edit" }</button>
-    //                     </div>
-    //                 }
-    //             })
-    //         }
-    //         </div>
-    //     </div>
-    // }
+    let next_theme = {
+        let theme_index = theme_index.clone();
+        let themes = themes.clone();
+        Callback::from(move |_| {
+            let next_index = (*theme_index + 1) % themes.len();
+            theme_index.set(next_index);
+
+            // print the current theme
+            web_sys::console::log_1(&format!("theme: {}", themes[next_index]).into());
+        })
+    };
+
+    let prev_theme = {
+        let theme_index = theme_index.clone();
+        let themes = themes.clone();
+        Callback::from(move |_| {
+            let prev_index = if *theme_index == 0 {
+                themes.len() - 1
+            } else {
+                *theme_index - 1
+            };
+            theme_index.set(prev_index);
+
+            // print the current theme
+            web_sys::console::log_1(&format!("theme: {}", themes[prev_index]).into());
+        })
+    };
+
     html! {
-    <div class="weight-app">
-        <div class="top-bar">
-            <button class="fetch-button" onclick={Callback::from(move |_| fetch_weights.emit(()))}>{ "Fetch Weights" }</button>
-            <button class="create-button" onclick={create_weight}>{ "Create Weight" }</button>
-        </div>
-        <div class="weight-list">
-        {
-            for weights.iter().map(|weight| {
-                let delete_callback = make_delete_callback(weight.weight_id);
-                let edit_callback = make_edit_callback(weight.weight_id);
-
-                html! {
-                    <div class="weight-item">
-                        <div class="weight-details">
-                            <p class="weight-id">{ format!("ID: {}", weight.weight_id) }</p>
-                            <p class="weight-date">{ format!("Date: {}", weight.measured_at) }</p>
-                            <p class="weight-kg">{ format!("{} kg", weight.weight_kg) }</p>
+        <div class={classes!("weight-app", themes[*theme_index])}>
+            <div class="top-bar">
+                <button class="prev-button" onclick={prev_theme}>{ "Previous Theme" }</button>
+                <span class={"theme-label"}>{ format!("Current Theme: {}", themes[*theme_index]) }</span>
+                <button class="next-button" onclick={next_theme}>{ "Next Theme" }</button>
+                <button class="fetch-button" onclick={Callback::from(move |_| fetch_weights.emit(()))}>{ "Fetch Weights" }</button>
+                <button class="create-button" onclick={create_weight}>{ "Create Weight" }</button>
+            </div>
+            <div class="weight-list">
+                { for weights.iter().map(|weight| {
+                    let delete_callback = make_delete_callback(weight.weight_id);
+                    let edit_callback = make_edit_callback(weight.weight_id);
+                    let local_time = chrono::Local.from_utc_datetime(&weight.measured_at);
+                    let formatted_time = local_time.format("%Y-%m-%d %H:%M").to_string();
+                    let weight_in_lbs = weight.weight_kg * 2.20462;
+                    html! {
+                        <div class="weight-item">
+                            <div class="weight-details">
+                                // <p class="weight-id">{ format!("ID: {}", weight.weight_id) }</p>
+                                <p class="weight-date">{ format!("{}", formatted_time) }</p>
+                                // <p class="weight-kg">{ format!("{:.1} kg", weight.weight_kg) }</p>
+                                <p class="weight-lbs">{ format!("{:.1} lbs", weight_in_lbs) }</p>
+                            </div>
+                            <div class="weight-actions">
+                                <button class="delete-button" onclick={delete_callback}>{ "Delete" }</button>
+                                <button class="edit-button" onclick={edit_callback}>{ "Edit" }</button>
+                            </div>
                         </div>
-                        <div class="weight-actions">
-                            <button class="delete-button" onclick={delete_callback}>{ "Delete" }</button>
-                            <button class="edit-button" onclick={edit_callback}>{ "Edit" }</button>
-                        </div>
-                    </div>
-                }
-            })
-        }
+                    }
+                })}
+            </div>
         </div>
-    </div>
-
-        }
+    }
 }
 
 fn main() {
